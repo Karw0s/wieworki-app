b5262e23f83cad3d910ba734e2ae8af4dc70d2f2
Michał Karwowski <michal.karwowski.0@gmail.com>
2020-11-10 18:27:10 +0100

    Add dummy text to question screen

--- lib/views/Question.dart
+++ lib/views/Question.dart
@@ -29,14 +29,21 @@ class Question extends StatelessWidget {
     double buttonTextSize;
     double buttonWidth;
     double buttonHeight;
+    double backWidth;
+    double backHeight;
+    String text = """Lorem ipsum dolor sit amet augue quis lacus. Vivamus malesuada fames ac nunc hendrerit sed, venenatis pede. Etiam imperdiet, nisl nulla ipsum wisi, eu fermentum ultrices posuere dui. Aliquam fermentum molestie egestas, nulla ultricies vitae, consequat hendrerit. Donec urna sem pede bibendum porttitor. Maecenas facilisis congue. Nam purus. Aenean venenatis interdum, lacus. Vivamus arcu turpis at eros. Sed lobortis urna. Nulla nec urna quis massa. Integer aliquet lacinia dignissim. Etiam ullamcorper. Mauris metus. Cras sit amet neque. Phasellus ultrices, gravida varius nec, nibh. Quisque nunc. Suspendisse nec erat velit in quam tristique bibendum quis, varius quis, vestibulum lectus elit, pulvinar augue. Sed ornare, erat id ipsum. Vestibulum ante ipsum scelerisque porttitor auctor, sapien auctor est, ut congue risus commodo est lacus ipsum dolor sit amet neque. Integer ultrices posuere, nibh ac turpis libero, pharetra varius. In tempus euismod. Integer egestas, dui non leo. Aenean dui vitae wisi placerat fringilla, vulputate faucibus. Sed posuere cubilia Curae, Vestibulum lacinia erat. Pellentesque habitant morbi tristique libero nec augue varius dui. Pellentesque quam at libero. Cras vitae tellus. Donec consectetuer adipiscing dictum a, euismod.""";
     if (screenWidth > 800) {
       buttonTextSize = 32;
-      buttonWidth = 300;
-      buttonHeight = 150;
+      buttonWidth = 250;
+      buttonHeight = 125;
+      backWidth = 100.0;
+      backHeight = 50;
     } else {
       buttonTextSize = 20;
-      buttonWidth = 200;
-      buttonHeight = 100;
+      buttonWidth = 150;
+      buttonHeight = 75;
+      backWidth = 88.0;
+      backHeight = 36;
     }
 
     var hintButton = MaterialButton(
@@ -86,13 +93,15 @@ class Question extends StatelessWidget {
         Navigator.pop(context);
         Navigator.pop(context);
       },
+      minWidth: backWidth,
+      height: backHeight,
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(18),
           side: BorderSide(color: Colors.red)),
       child: Text("Powrót"),
     );
     List<Widget> content = List();
-    content.add(Text("Pytanie"));
+    content.add(Text(text));
 
     if (screenWidth < 800) {
       content.add(answerButton);
